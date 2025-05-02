e3dd721f3c621611e5122dda73341a3bd814763c
Partha Das <partha.das@german-uds.de>
2025-05-02 15:51:15 +0200

    fix: update Next.js config for development mode and adjust Tailwind config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // enables static exports
-  basePath: '/innovation-website', // replace with your repository name
   reactStrictMode: true,
   images: {
-    unoptimized: true, // required for static export
+    unoptimized: true,
   },
 }
 
