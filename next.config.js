3f4c8bcead22294d996dd5dfe53a361021ca901f
Partha Das <partha.das@german-uds.de>
2025-05-02 16:06:55 +0200

    fix: update GitHub Pages configuration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
-  basePath: process.env.NODE_ENV === 'production' ? '/innovation-website' : undefined,
+  basePath: process.env.NODE_ENV === 'production' ? '' : undefined,
+  assetPrefix: process.env.NODE_ENV === 'production' ? '.' : undefined,
   reactStrictMode: true,
   images: {
     unoptimized: true,
